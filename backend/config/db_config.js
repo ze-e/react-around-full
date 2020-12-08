2015655ad55040648c6feb9df940820777a82ec5
zrr <zrexrodriguez@gmail.com>
2020-12-07 16:47:39 -0800

    fixed default values

--- backend/config/db_config.js
+++ backend/config/db_config.js
@@ -1,5 +1,5 @@
 //config
 require('dotenv').config(); 
 
-const {DB_USER = 'admin', DB_PASSWORD = 'testing123', DB_NAME = 'testDB'} = process.env;
+const {DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 module.exports.DATABASE = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.szyal.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
