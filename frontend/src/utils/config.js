60e878d10a649c51d81e2a003619226f9e378a4e
zrr <zrexrodriguez@gmail.com>
2020-12-08 22:40:44 -0800

    configured for heroku

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -2,5 +2,5 @@ require('dotenv').config({ path: '../../../' });
 const path = require('path');
 const baseURL = path.resolve(__dirname,'../../..','backend','server.js');
 
-module.exports = baseURL;
 //export const baseURL = process.env.PUBLIC_URL;
+export {baseURL};
