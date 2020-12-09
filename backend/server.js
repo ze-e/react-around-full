a931e047107723146fb46fc5a6d32ca47b30bd5e
zrr <zrexrodriguez@gmail.com>
2020-12-08 16:46:55 -0800

    configured for heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 //config
 require('dotenv').config({ path: '../' }); 
 
@@ -55,6 +57,14 @@ res.status(statusCode).send({
 });
 //errorlogger
 app.use(errorLogger);
+
+//serve static assets 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 //server
 app.listen(PORT, () => {
