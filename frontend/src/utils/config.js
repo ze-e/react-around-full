ef1e9d288a1650683798f5674cea60fe3aef2b41
zrr <zrexrodriguez@gmail.com>
2020-12-09 19:15:07 -0800

    add db defaults

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,3 @@
-//config
 require('dotenv').config(); 
-const {DB_USER='admin', DB_PASSWORD='testing123', DB_NAME='testDB'} = process.env;
-module.exports.DATABASE = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.szyal.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const baseURL = process.env.PUBLIC_URL || 'https://api.zrexrodriguez.students.nomoreparties.site';
+export default baseURL;
